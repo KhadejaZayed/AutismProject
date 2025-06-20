ef4f07153504b5f6496e52b2b4eb6e554f203f19
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-20 22:22:07 +0300

    add changePassword route to authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -4,7 +4,10 @@ const authController = require("../Controllers/authController");
 
 // TODO: Add Logout Function
 
-router.post("/sign/user", authController.signUser);
-router.post("/login/user", authController.loginUser);
+router.post("/sign",           authController.signUser);
+router.post("/login",          authController.loginUser);
+
+router.post('/changePassword', authController.changePassword);
+
 
 module.exports = router;
