93834d404ddc520750b789df59080173a209aab2
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-21 21:13:28 +0300

    Update and rename arLetterController.js to letterController.js
    make it reusable to eliminate duplication for English letters

--- Controllers/letterController.js
+++ Controllers/letterController.js
@@ -1,18 +1,17 @@
-const Letter = require('../Models/arLetterModel');
 
-exports.getAllLetters = async (req, res) => {
+//Model comes from route which define it want arModel or enModel
+exports.getAllLetters = (Model)=> async (req, res) => {
   try {
-    const letters = await Letter.find();
+    const letters = await Model.find();
     res.json(letters);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-
-exports.getLetterById = async (req, res) => {
+exports.getLetterById = (Model)=> async (req, res) => {
   try {
-    const letter = await Letter.findById(req.params.id);
+    const letter = await Model.findById(req.params.id);
     if (!letter)
         return res.status(404).json({ message: 'Letter Not Found!' });
     
@@ -22,9 +21,8 @@ exports.getLetterById = async (req, res) => {
   }
 };
 
-
-// exports.createLetter = async (req, res) => {
-//   const newLetter = new Letter(req.body);
+// exports.createLetter = (Model)=> async (req, res) => {
+//   const newLetter = new Model(req.body);
 //   try {
 //     const savedLetter = await newLetter.save();
 //     res.status(201).json(savedLetter);
