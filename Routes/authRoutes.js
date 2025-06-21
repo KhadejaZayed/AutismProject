10ef5d059f4d0946ed3c23a704b6077fd417eec7
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-21 22:53:27 +0300

    Update authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,8 +2,6 @@ const express  = require("express");
 const router   = express.Router();    
 const authController = require("../Controllers/authController");
 
-// TODO: Add Logout Function
-
 router.post("/sign",           authController.signUser);
 router.post("/login",          authController.loginUser);
 
