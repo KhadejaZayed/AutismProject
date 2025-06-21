8a89a143a16e1fa604d7708564917a8a3b2874af
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-21 21:16:10 +0300

    Update and rename arLetterRoute.js to letterRoute.js
    add English routes to the same file so it can be letterRoutes

--- Routes/arLetterRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const arLetterController = require('../Controllers/arLetterController');
-
-router.get('/', arLetterController.getAllLetters);
-// router.post('/', arLetterController.createLetter);
-router.get('/:id', arLetterController.getLetterById);
-
-module.exports = router;
---
+++ Routes/letterRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const arModel = require('../Models/arLetterModel');
+const enModel = require('../Models/enLetterModel');
+const letterController = require('../Controllers/letterController');
+
+//Arabic Letters routes
+router.get('/arabic',     letterController.getAllLetters(arModel));
+router.get('/arabic/:id', letterController.getLetterById(arModel));
+// router.post('/arabic', letterController.createLetter(arModel));
+
+//English Lettters routes
+router.get('/english',     letterController.getAllLetters(enModel));
+router.get('/english/:id', letterController.getLetterById(enModel));
+// router.post('/english', letterController.createLetter(enModel));
+
+module.exports = router;
