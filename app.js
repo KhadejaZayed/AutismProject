8c4cd77bafa3c9922eed9e24a74d54d291dbb4dc
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-21 22:05:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const auth     = require("./Routes/authRoutes"),
       diary    = require('./Routes/diaryRoutes'),
       question = require('./Routes/questionRoutes'),
       avatar   = require('./Routes/avatarRoutes'),
-      letter   = require('./Routes/letterRoute');
-
+      letter   = require('./Routes/letterRoute'),
+      number   = require('./Routes/numberRoute');
+      
 connect.then(()=>{
     console.log("DB connects successfully");
     app.listen(port, ()=>{
@@ -29,18 +30,19 @@ app.use(express.json()); //convert data into json format
 app.use(express.urlencoded({extended:false}));
 app.set('view engine','ejs'); //use ejs as the view engine
 app.use(cors());
+
 //just for testing
 app.use('/uploads', express.static('public/uploads'));
-app.use('/assets', express.static('public/assets'));
+app.use('/assets',  express.static('public/assets'));
 
-
-app.use('/auth',auth);
+app.use('/auth',     auth);
 app.use('/coins',    coins);
-app.use('/child',    child);
+app.use('/child',    child); 
 app.use('/diary',    diary);
 app.use('/question', question);
 app.use('/avatar',   avatar);
 app.use('/letter',   letter);
+app.use('/number',   number);
 
 app.use((request, response) => {
     response.status(404).json({ message: "Not Found" });
