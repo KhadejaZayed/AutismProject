9150958a542257dbd7780d081df2591998050534
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-06-28 20:44:31 +0300

    Update coinsRoutes.js

--- Routes/coinsRoutes.js
+++ Routes/coinsRoutes.js
@@ -3,10 +3,10 @@ const router     = express.Router();
 const coinsController = require("../Controllers/coinsController");
 const authMiddleware = require('../Middleware/authMiddleware');
 
-router.get("/",authMiddleware,coinsController.getCoins);
-router.post("/add",authMiddleware, coinsController.addCoins);
-router.post("/deduct",authMiddleware, coinsController.deductCoins);
-//router.post("/reset", userController.resetCoins);
-router.post("/levelup",authMiddleware, coinsController.levelUp);
+router.get("/",         authMiddleware, coinsController.getCoins);
+router.post("/add",     authMiddleware, coinsController.addCoins);
+router.post("/deduct",  authMiddleware, coinsController.deductCoins);
+router.post("/levelup", authMiddleware, coinsController.levelUp);
+// router.post("/reset",   authMiddleware, coinsController.resetCoins);
 
 module.exports = router;
