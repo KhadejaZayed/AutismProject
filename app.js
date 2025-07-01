e80ae010ee8f92a1bb1ffd42ce7abb93df22fc6e
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2025-07-01 23:27:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express    = require('express'),
       app        = express(),
       cors       = require("cors"),
       port       = process.env.PORT || 8080,
-      mongoose   = require('mongoose'), connect = mongoose.connect("mongodb://localhost:27017/");
+      mongoose   = require('mongoose'), connect = mongoose.connect("mongodb://127.0.0.1:27017/BrightpathDB");
 
 mongoose.set("strictQuery", false);
 
